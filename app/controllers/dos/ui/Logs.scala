aa4470400d52fba2cc5054db21f1fae2a6e24ae1
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-10-30 19:04:18 +0100

    View improvements
    Only displaying decently-sized logs at once, and adding a way to show the whole log.
    Linking browed directories.

--- app/controllers/dos/ui/Logs.scala
+++ app/controllers/dos/ui/Logs.scala
@@ -6,6 +6,7 @@ import play.mvc.Controller
 import org.bson.types.ObjectId
 import play.mvc.results.Result
 import com.mongodb.casbah.commons.MongoDBObject
+import com.novus.salat.dao.SalatMongoCursor
 
 /**
  *
@@ -15,12 +16,25 @@ import com.mongodb.casbah.commons.MongoDBObject
 object Logs extends Controller with Extensions {
 
   def list(taskId: ObjectId, lastCount: Int): Result = {
-    if(lastCount != null && lastCount > 0) {
-      val logs = Log.find(MongoDBObject("task_id" -> taskId)).sort(MongoDBObject("date" -> 1)).skip(lastCount + 1)
-      Json(Map("logs" -> logs))
+    val cursor: SalatMongoCursor[Log] = Log.find(MongoDBObject("task_id" -> taskId)).sort(MongoDBObject("date" -> 1))
+    val (logs, skipped) = if(lastCount != null && lastCount > 0) {
+      if(cursor.count - lastCount > 100) {
+        (cursor.skip(cursor.count - 100), true)
+      } else {
+        (cursor.skip(lastCount + 1), false)
+      }
     } else {
-      val logs = Log.find(MongoDBObject("task_id" -> taskId)).sort(MongoDBObject("date" -> 1))
-      Json(Map("logs" -> logs))
+      if(cursor.count > 100) {
+        (cursor.skip(cursor.count - 100), true)
+      } else {
+        (cursor, false)
+      }
     }
+    Json(Map("logs" -> logs, "skipped" -> skipped))
+  }
+
+  def view(taskId: ObjectId): Result = {
+    val cursor: SalatMongoCursor[Log] = Log.find(MongoDBObject("task_id" -> taskId)).sort(MongoDBObject("date" -> 1))
+    Text(cursor.map(log => log.date + "\t" + log.level.name.toUpperCase  + "\t" + log.message).mkString("\n"))
   }
 }
